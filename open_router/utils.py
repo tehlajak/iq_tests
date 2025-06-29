7421cb5ed2e719d6743d6029844ae8fc293bae78
Jakub Tehlar <tehlarjakub@gmail.com>
2025-06-29 11:23:46 +0200

    implement save_json

--- open_router/utils.py
+++ open_router/utils.py
@@ -1,4 +1,13 @@
 import base64
+import json
 def encode_image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
+
+def save_json(fname: str, content):
+    try:
+        with open(fname, "w") as f:
+            json.dump(content, f, indent=4)
+        print(f"Contents saved to {fname}!")
+    except:
+        print(f"Could not save to {fname}")
