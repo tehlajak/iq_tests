907c60799fedfd13794c8319307c37c2b8844958
Jakub Tehlar <tehlarjakub@gmail.com>
2025-06-29 13:20:21 +0200

    add structured output option

--- open_router/open_router_interface.py
+++ open_router/open_router_interface.py
@@ -1,10 +1,15 @@
 import json
 import requests
 class OpenRouter():
-    def __init__(self, api_key: str, model_name: str, completions_url: str = "https://openrouter.ai/api/v1/chat/completions"): 
+    def __init__(self,
+                api_key: str,
+                model_name: str,
+                structured_output: bool=True,
+                completions_url: str = "https://openrouter.ai/api/v1/chat/completions"): 
         self.api_key = api_key
         self.completions_url = completions_url
         self.model = model_name
+        self.structured_output = structured_output
         self.headers = {
                 "Authorization": f"Bearer {api_key}",
                 "Content-Type": "application/json"
@@ -16,6 +21,34 @@ class OpenRouter():
             "model": self.model,
             "messages": [message] # Must be a list
         }
+
+        if self.structured_output:
+            payload = {
+                "model": self.model,
+                "messages": [message], # Must be a list
+                "response_format": {
+                    "type": "json_schema",
+                    "json_schema": {
+                        "name": "iq_test_reasoning",
+                        "strict": True,
+                        "schema": {
+                            "type": "object",
+                            "properties": {
+                                "reasoning": {
+                                    "type": "string",
+                                    "description": "Description of the reasoning process"
+                                },
+                                "prediction": {
+                                    "type": "string",
+                                    "description": "Selected answer"
+                                }
+                            },
+                            "required": ["reasoning", "prediction"],
+                            "additionalProperties": False
+                        }
+                    }
+                }
+            }
 
         response = requests.post(
             url = self.completions_url,
