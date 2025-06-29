650444dfc9fcbc096adbe0ce0551a9573317a65a
Jakub Tehlar <tehlarjakub@gmail.com>
2025-06-29 12:16:48 +0200

    update

--- open_router/utils.py
+++ open_router/utils.py
@@ -1,5 +1,7 @@
 import base64
 import json
+import pandas as pd
+import os
 def encode_image_to_base64(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
@@ -11,3 +13,12 @@ def save_json(fname: str, content):
         print(f"Contents saved to {fname}!")
     except:
         print(f"Could not save to {fname}")
+
+def load_csv(fname: str, separator:str = ",") -> pd.DataFrame:
+    data = pd.read_csv(fname, sep=separator)
+    return data
+
+def ensure_dir(dir_path: str):
+    """Create a directory if it does not exist."""
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
